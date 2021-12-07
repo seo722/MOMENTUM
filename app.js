fcf9ece2c166fd579d2ccfc1bf2515e332dd9e2f
seo722 <95078290+seo722@users.noreply.github.com>
2021-12-07 23:05:56 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 10;
+const b = 2;
+let myName = "인서";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "seo";
+console.log("your new name is " + myName);
