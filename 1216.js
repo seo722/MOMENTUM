20e9a1ea883295cef2166034859bef2884aa8403
seo722 <95078290+seo722@users.noreply.github.com>
2021-12-16 23:55:38 +0900

    css in javascript

---
+++ 1216.js
@@ -0,0 +1,29 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleTitleClick);
+
+//
+
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
+  } else {
+    h1.classList.add(clickedClass);
+  }
+}
+
+h1.addEventListener("click", handleTitleClick);
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const a = 10;
-const b = 2;
-let myName = "인서";
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+function handleTitleClick() {
+  h1.classList.toggle("clicked");
+}
 
-myName = "seo";
-console.log("your new name is " + myName);
+h1.addEventListener("click", handleTitleClick);
+
+//toggle ===
+//const clickedClass = "clicked";
+//if (h1.classList.contains(clickedClass)) {
+//  h1.classList.remove(clickedClass);
+//} else {
+//  h1.classList.add(clickedClass);
+//}
+//toggle === classList에 내가 원하는 class가 있는지를 확인해서
+//없으면 넣고 있으면 제거.
