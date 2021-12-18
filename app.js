0ee5ea23089ad08dc243541248437da2f5ee2897
seo722 <95078290+seo722@users.noreply.github.com>
2021-12-19 02:06:37 +0900

    username 변수를 저장하는 법

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  //greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  //string 안에 변수를 넣고 싶을 때
-  //`string ${변수}`
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
 
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-//default behavior 기본 동작
-//submit의 기본 동작 : 새로고침
-//a(링크)의 기본 동작 : 링크로 페이지 옮기기
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
