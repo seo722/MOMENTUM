2ae061e87e017059eb5e7fcc71287f2065499770
seo722 <95078290+seo722@users.noreply.github.com>
2021-12-23 16:38:46 +0900

    백ㄱ라운드컬러

---
+++ backcolor.js
@@ -0,0 +1,31 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+const button = document.querySelector("#color");
+
+function clicker() {
+  const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+  const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+  const bg = document.querySelector("body");
+  bg.style.background = `linear-gradient(to right, ${randomColor1}, ${randomColor2})`;
+}
+
+clicker();
+button.addEventListener("click", clicker);
