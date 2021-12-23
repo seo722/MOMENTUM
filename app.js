0657d04f64ff86afc2db2e90cc6f1b5039fb7849
seo722 <95078290+seo722@users.noreply.github.com>
2021-12-23 12:39:27 +0900

    크리스마스 디데이

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const clockTitle = document.querySelector("#x-clock");
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+function getTime() {
+  const xmasDay = new Date(`${new Date().getFullYear()}-12-25:00:00:00+0900`);
+  const now = new Date();
+  const difference = new Date(xmasDay - now);
 
-function onLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  const secondsInMs = Math.floor(difference / 1000);
+  const minutesInMs = Math.floor(secondsInMs / 60);
+  const hoursInMs = Math.floor(minutesInMs / 60);
+  const days = Math.floor(hoursInMs / 24);
+
+  const seconds = secondsInMs % 60;
+  const minutes = minutesInMs % 60;
+  const hours = hoursInMs % 24;
+
+  const daysStr = `${days < 10 ? `0${days}` : days}d`;
+  const hoursStr = `${hours < 10 ? `0${hours}` : hours}h`;
+  const minutesStr = `${minutes < 10 ? `0${minutes}` : minutes}m`;
+  const secondsStr = `${seconds < 10 ? `0${seconds}` : seconds}s`;
+
+  clockTitle.innerHTML = `${daysStr} ${hoursStr} ${minutesStr} ${secondsStr}`;
 }
+getTime();
+setInterval(getTime, 1000);
+//내가 한거 ㅋ
+//const xClock = document.querySelector("#x-clock");
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-}
+//function getClock() {
+//const xMas = new Date("December 25, 2021 00:00:00");
+//const now = new Date();
+//const days = xMas.getDay() - now.getDay() - 1;
+//const hours = 23 - now.getHours();
+// const minutes = 59 - now.getMinutes();
+//const seconds = 59 - now.getSeconds();
+// xClock.innerText = `${days}일 ${hours}시간 ${minutes}분 ${seconds}초`;
+//}
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
-if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
-} else {
-  paintGreetings(savedUsername);
-}
+//getClock();
+//setInterval(getClock, 1000);
